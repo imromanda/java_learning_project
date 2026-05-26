c3819f69d036adfccc488a402501d90740f52e4a
ImRomanda <imromanda@mail.com>
2026-05-26 16:44:22 +0200

    Array analysis exercise

--- src/com/curso/fundamentos/ejercicios_basicos/ArrayAnalisis.java
+++ src/com/curso/fundamentos/ejercicios_basicos/ArrayAnalisis.java
@@ -2,4 +2,40 @@ package com.curso.fundamentos.ejercicios_basicos;
 
 public class ArrayAnalisis {
 
+	
+	
+	/*
+	 * 1. Crear un Array de 5 elementos de tipo entero
+	 * 2. Calcularemos la media de los items (elementos del array)
+	 * 3. Recuperaremos el valor más alto (el mayor)
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		int[] numeros = {
+			23, -63, 69, 21, 82
+		};
+		
+		//2. Calcularemos la media de los items
+		
+		int suma = 0; 
+		double media = 0;
+		
+		for (int numero:numeros) {
+			suma = suma + numero;
+			System.out.println("Paso a paso " + suma);
+		} 
+		
+		System.out.println("Suma total " + suma);
+		
+		media = (suma/numeros.length);
+		System.out.println("Media " + media);
+
+//		for (int i = 0; i < numeros.length; i++);
+		
+		
+		
+	}
+	
 }
+
