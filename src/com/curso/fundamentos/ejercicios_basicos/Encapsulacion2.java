bc71acb838a2732a9ec2d9a730f14fade2a6f9b2
ImRomanda <imromanda@mail.com>
2026-05-29 18:26:20 +0200

    Ejercicio encapsulación terminado

--- src/com/curso/fundamentos/ejercicios_basicos/Encapsulacion2.java
+++ src/com/curso/fundamentos/ejercicios_basicos/Encapsulacion2.java
@@ -19,11 +19,33 @@ import com.curso.fundamentos.utils.Entrada;
 
 public class Encapsulacion2 {
 	
+	
 	public static void limpiarConsola() {
-		for (int i = 0; i < 1; i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println();
 		}
 	}//Fin limpiarConsola()
+	
+	
+	public static void listarCuentas(ArrayList<CuentaBancaria> cuentas) {
+		int count = 0;
+		//Listar cuentas
+		if (cuentas.isEmpty()) {
+				System.out.println("\nNo hay nada que mostrar. Añade primero una cuenta");
+		} else {
+				System.out.println("\n======LISTA DE CUENTAS======");
+				count = 0;
+				for(CuentaBancaria cuenta:cuentas) {
+					
+					System.out.println("\nCuenta nº " + count + ""
+							+ "\nTitular de la cuenta: " + cuenta.getTitular() + 
+							"\nSaldo de la cuenta: " + cuenta.getSaldo());
+						count++;
+					}//Fin for
+				} //fin else
+			
+			}//Fin case 3
+	
 	
 	//=================================================================================
 	//MENÚ OPERACIONES 
@@ -31,12 +53,15 @@ public class Encapsulacion2 {
 
 	
 	public static void menuOperaciones(CuentaBancaria cuenta) {
+
+
+
 		
 
 		int miOpcion;
 		
 		do {
-			System.out.println("\nTitular actual: " + cuenta.getTitular());
+//			System.out.println("\nTitular actual: " + cuenta.getTitular());
 			miOpcion = Entrada.enteroRango("\n====OPERACIONES SOBRE CUENTA BANCARIA===="
 							+ "\n 1. Consultar saldo"
 							+ "\n 2. Ingresar saldo"
@@ -57,6 +82,8 @@ public class Encapsulacion2 {
 	}//Fin menuOperaciones
 	
 	public static void accionCuenta(int caso, CuentaBancaria c) {
+		limpiarConsola();
+
 		
 		switch (caso) {
 			case 1 -> {
@@ -80,7 +107,9 @@ public class Encapsulacion2 {
 //				c.setTitular(nuevoTitular);
 			}
 			case 0 -> {
-				System.out.println("\nSaliendo del programa. Gracias por jugar.");
+				System.out.println("\n============================="
+						+ "Saliendo del programa. Gracias por jugar."
+						+ "=============================");
 			}
 
 		} //Fin del switch
@@ -97,26 +126,83 @@ public class Encapsulacion2 {
 		ArrayList<CuentaBancaria> cuentas = new ArrayList<CuentaBancaria> ();
 		
 		//Variable de tipo CuentaBancaria donde guardaré la cuenta seleccionada
-		CuentaBancaria cuentaActiva = null;
+		 //CuentaBancaria cuentaActiva = null;
 		
 		//=================================================================================
 		//MENÚ PRINCIPAL 
 		//=================================================================================
+		limpiarConsola();
 
 		int miOpcion;
+
 				
 				do {
-					miOpcion = Entrada.enteroRango("\n====MENÚ PRINCIPAL===="
+					miOpcion = Entrada.enteroRango("\n=======MENÚ PRINCIPAL======="
 									+ "\n 1. Crear nueva cuenta"
 									+ "\n 2. Seleccionar cuenta"
 									+ "\n 3. Listar cuentas"
-									+ "\n 4. Eliminar cuenta"
 									+ "\n 0. Salir"
 									+ "  "
-									+ "  ", 0, 4);
+									+ "  ", 0, 3);
 					
 					limpiarConsola();
+					switch (miOpcion) {
+						case 1 -> {
+							//Crear nueva cuenta
+							String miTitular = Entrada.texto("\nIntroduce "
+									+ "el Titular de la cuenta: ");
+							Double miSaldo = Entrada.doble("\nIntroduce "
+									+ "el Saldo inicial: ");
+							//Creamos el item (objeto) de ArrayList cuentas
+							//instanciando la clase CuentaBancaria
+							//cuentas = nombre del arrayList
+							//.add = método de arraylist para añadir al final
+							//new CuentaBancaria = nuevo objeto tipo CuentaBancaria
+							//Mandamos dos parámetros que son miTitular y miSaldo
+							cuentas.add( new CuentaBancaria(miTitular, miSaldo));
+							
+							System.out.println("\nCuenta del titular "
+									 + miTitular + " con " + miSaldo + 
+									"€ creada correctamente");
+							}
+						case 2 -> {
+							listarCuentas(cuentas);
+							 miOpcion = Entrada.entero("\nEscribe el índice de la cuenta para operar: ");
+								
+							 //Obtiene del ArrayList la cuenta seleccionada por el usuario
+							// usando el índice introducido y la envía al menú de operaciones
+							menuOperaciones(cuentas.get(miOpcion));
+						}
+//							//Seleccionar cuentas
+//							
+//							if (cuentas.isEmpty()) {
+//									System.out.println("\nNo hay cuentas para seleccionar. "
+//											+ "Añade primero una pulsando '1'");
+//									break;
+//								} else {
+//									System.out.println("\n======MIS CUENTAS======");
+//									count = 0;
+//									for(CuentaBancaria cuenta:cuentas) {
+//									
+//										System.out.println("\nCuenta nº " + count + ""
+//												+ "\nTitular de la cuenta: " + cuenta.getTitular() + 
+//												"\nSaldo de la cuenta: " + cuenta.getSaldo());
+//											count++;
+//										}//Fin for
+//								 miOpcion = Entrada.entero("\nEscribe el índice de la cuenta para operar: ");
+//								
+//								 //Obtiene del ArrayList la cuenta seleccionada por el usuario
+//								// usando el índice introducido y la envía al menú de operaciones
+//								menuOperaciones(cuentas.get(miOpcion));
+//
+//									} //fin else
+//							}//Fin Case 2
+						
+						case 3 -> {
+							listarCuentas(cuentas);
+						}
 					
+					}//Fin de SWITCH
 				
 				} while (miOpcion != 0 );
 		
@@ -124,11 +210,11 @@ public class Encapsulacion2 {
 		
 
 		
-		String miTitular = Entrada.texto("\nIntroduce el Titular de la cuenta: ");
-		Double miSaldo = Entrada.doble("\nIntroduce el Saldo inicial de la cuenta: ");
+//		String miTitular = Entrada.texto("\nIntroduce el Titular de la cuenta: ");
+//		Double miSaldo = Entrada.doble("\nIntroduce el Saldo inicial de la cuenta: ");
+//		
 		
-		
-		CuentaBancaria miCuenta = new CuentaBancaria(miTitular, miSaldo);
+	//	CuentaBancaria miCuenta = new CuentaBancaria(miTitular, miSaldo);
 //		
 //		System.out.println("\nNombre del titular: " + miCuenta.getTitular() + " ");
 //		System.out.println("\nSaldo de la cuenta: " + miCuenta.getSaldo() + " ");
